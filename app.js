16d5583e08aaa9899c80785aa56d65b24ae0d3da
Hernan Di Tano <hditano@gmail.com>
2023-06-25 14:21:54 +0200

    added basic new note container

--- app.js
+++ app.js
@@ -1,18 +1,29 @@
-var Note = /** @class */ (function () {
-    function Note(firstName, lastName) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
-    return Note;
-}());
 document.addEventListener("DOMContentLoaded", function () {
     var submitButton = document.getElementById('btn-click');
     if (submitButton !== null) {
         submitButton.addEventListener('click', function () {
             var firstName = document.getElementById("firstName_text").value;
             var lastName = document.getElementById("lastName_text").value;
-            var newNote = new Note(firstName, lastName);
-            console.log(newNote);
+            var note = new newNote(firstName, lastName);
+            note.AddNote();
         });
     }
 });
+var newNote = /** @class */ (function () {
+    function newNote(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    newNote.prototype.AddNote = function () {
+        var elementFirstName = document.createElement('p');
+        var elementLastName = document.createElement('p');
+        elementFirstName.innerText = this.firstName ? this.firstName : '';
+        elementLastName.innerText = this.lastName ? this.lastName : '';
+        var noteContainer = document.getElementById("new-notes");
+        if (noteContainer !== null) {
+            noteContainer.appendChild(elementFirstName);
+            noteContainer.appendChild(elementLastName);
+        }
+    };
+    return newNote;
+}());
--- app.ts
+++ app.ts
@@ -1,13 +1,3 @@
-class Note {
-    firstName: string | undefined;
-    lastName: string | undefined;
-
-    constructor(firstName: string | undefined, lastName: string | undefined) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
-}
-
 document.addEventListener("DOMContentLoaded", () => {
     const submitButton = document.getElementById('btn-click');
     
@@ -16,8 +6,32 @@ document.addEventListener("DOMContentLoaded", () => {
 
             const firstName = (document.getElementById("firstName_text") as HTMLInputElement).value;
             const lastName = (document.getElementById("lastName_text") as HTMLInputElement).value;
-            let newNote = new Note(firstName, lastName);
-            console.log(newNote);
+            let note = new newNote(firstName, lastName);
+            note.AddNote();
         })
     }
 })
+
+class newNote {
+    firstName: string | undefined;
+    lastName: string | undefined;
+
+    constructor(firstName: string | undefined, lastName: string | undefined){
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    AddNote() {
+        const elementFirstName = document.createElement('p');
+        const elementLastName = document.createElement('p');
+
+        elementFirstName.innerText = this.firstName ? this.firstName: '';
+        elementLastName.innerText = this.lastName ? this.lastName: '';
+
+        const noteContainer = document.getElementById("new-notes");
+        if(noteContainer !== null) {
+            noteContainer.appendChild(elementFirstName);
+            noteContainer.appendChild(elementLastName);
+        }
+    }
+}
